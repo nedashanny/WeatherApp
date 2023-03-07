3b5142d0df38eb57a352a333e1603290d890dfec
neadashanny <neadashanny@gmail.com>
2023-03-07 13:57:41 +0330

    show current weather

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,30 @@
-<script >
-
+<script>
+  import axios from "axios";
+  export default{
+    data(){
+      return{
+        ipAdress:null,
+        currentconditions:null,
+      }
+    },
+    beforeMount(){
+      // get user ip
+      axios.get('https://api.ipify.org?format=json')
+          .then(response=>{
+            this.ipAdress=response.data.ip
+          })
+    },
+    mounted(){
+      // find user location
+      axios.get('http://dataservice.accuweather.com/locations/v1/cities/ipaddress?q='+this.ipAdress+'&apikey=gnT2YKAwjOYiOGoR2cG0lLMXVyW2JUsA')
+          .then(response=>{
+            axios.get('http://dataservice.accuweather.com/currentconditions/v1/'+response.data.Key+'?apikey=gnT2YKAwjOYiOGoR2cG0lLMXVyW2JUsA')
+                .then(response=>{
+                  this.currentconditions=response
+                })
+          })
+    }
+  }
 </script>
 
 <template>
@@ -14,13 +39,12 @@
   </nav>
   <div class="container">
     <p class="fs-3 text-capitalize fw-bold">current</p>
-    <div class="card m-1" style="width: 10rem;">
-      <img src="./assets/icons/1-s.png" class="card-img-top mt-1" alt="...">
+    <div v-if="currentconditions!=null" class="card m-1" style="width: 10rem;">
+      <img :src="`src/assets/icons/${currentconditions.data[0].WeatherIcon}-s.png`" class="card-img-top mt-1" alt="...">
       <div class="card-body">
-        <h5 class="card-title">Cloudy</h5>
-        <p class="fs-4">17 C</p>
-        <p class="text-muted fs-6">01/11/12 - 20:20</p>
-        <a href="#" class="btn btn-primary text-capitalize">view website</a>
+        <h5 class="card-title">{{ currentconditions.data[0].WeatherText }}</h5>
+        <p class="fs-4">{{ currentconditions.data[0].Temperature.Metric.Value + " "+  currentconditions.data[0].Temperature.Metric.Unit}}</p>
+        <a :href="currentconditions.data[0].Link" class="btn btn-primary text-capitalize">view website</a>
       </div>
     </div>
     <p class="fs-3 text-capitalize fw-bold">hourly</p>
