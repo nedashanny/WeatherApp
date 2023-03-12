1dcd69c4f0bc85c8182cd8813f57c59d33527696
neadashanny <neadashanny@gmail.com>
2023-03-12 17:57:05 +0330

    show carousel caption taxt black when its day and white when its night

--- src/components/CurrentWeather.vue
+++ src/components/CurrentWeather.vue
@@ -3,6 +3,11 @@
         props:{
             title:String,
             weather:Array,
+        },
+        methods:{
+          cardBodyText(isDay){
+            return isDay?'black':'white'
+          }
         }
     }
 </script>
@@ -11,7 +16,7 @@
     <div class="w-100 overflow-scroll" style="white-space: nowrap;">
       <div class="card m-1 d-inline-block" style="width: 13rem;" v-for="(weather,index) in weather" :key="index" :class="{'bg-dark':!weather.IsDayTime}">
         <img :src="`src/assets/icons/${weather.WeatherIcon}-s.png`" class="card-img-top mt-1" alt="...">
-        <div class="card-body" :class="{'text-white':!weather.IsDayTime}">
+        <div class="card-body" :style="{'color':cardBodyText(weather.IsDayTime)+'!important'}">
           <h5 class="card-title">{{weather.WeatherText}}</h5>
           <p class="fs-4">{{ weather.Temperature.Metric.Value+"\u00B0" + " " + weather.Temperature.Metric.Unit}}</p>
           <a :href="weather.Link" class="btn btn-primary text-capitalize">view website</a>
